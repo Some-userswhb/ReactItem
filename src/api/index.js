0cf3fc641e66cddf22ca16d462fcc58998c2245a
wubinbin <981947064@qq.com>
2018-11-02 19:51:54 +0800

    React项目 第一次提交

---
+++ src/api/index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by 98194 on 2018/10/31.
+ */
+
+//发送请求
+
+import ajax from './ajax';
+
+//请求登录的函数
+export const reqLogin = data => ajax('/login', data, 'POST');
+//请求注册的函数
+export const reqRegister = data => ajax('/register', data, 'POST');
